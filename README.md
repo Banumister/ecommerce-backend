d4b7ee71c7d3134ca9ff13e665bf81c621e7bfa9
Raj Patil <rajpatil22338@gmail.com>
2024-10-19 16:45:45 +0530

    project packages added

--- README.md
+++
@@ -1,2 +0,0 @@
-# ecommerce-backend
-This is Repository of  ecommerce application backend code with spring boot and MySQL database using Maven.
---
+++ ecommrece-application-backend/src/main/java/com/vw/core/EcommreceApplicationBackendApplication.java
@@ -0,0 +1,13 @@
+package com.vw.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommreceApplicationBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommreceApplicationBackendApplication.class, args);
+	}
+
+}
