19ae3fa3788092d0aedf529b9283552b89a65a27
Raj Patil <rajpatil22338@gmail.com>
2024-10-19 17:23:29 +0530

    changes done

--- src/main/java/com/vw/core/entity/User.java
+++ src/main/java/com/vw/core/entity/User.java
@@ -1,9 +1,6 @@
 package com.vw.core.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,6 +10,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@Entity
 @Table(name = "user")
 public class User {
 
