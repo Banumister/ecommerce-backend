a7e1f98f46a4708357a71eec08a5350fb0e4a0d9
Raj Patil <rajpatil22338@gmail.com>
2024-10-19 16:58:58 +0530

    commented contextLoads in test class

--- ecommrece-application-backend/src/test/java/com/vw/core/EcommreceApplicationBackendApplicationTests.java
+++ ecommrece-application-backend/src/test/java/com/vw/core/EcommreceApplicationBackendApplicationTests.java
@@ -5,9 +5,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class EcommreceApplicationBackendApplicationTests {
+/*
 
 	@Test
 	void contextLoads() {
 	}
+*/
 
 }
